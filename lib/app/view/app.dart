1109367a18191a02a959fff59cf2a9f317137423
mehmet fiskindal <developersailor@gmail.com>
2025-04-19 00:47:38 +0300

    remove unused codes

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,11 +1,6 @@
-import 'package:audioplayers/audioplayers.dart';
-import 'package:fietris/l10n/l10n.dart';
-import 'package:fietris/loading/loading.dart';
-import 'package:flame/cache.dart';
-import 'package:flame/game.dart';
 import 'package:fietris/game/fietris_game.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
@@ -13,17 +8,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => PreloadCubit(
-            Images(prefix: ''),
-            AudioCache(prefix: ''),
-          )..loadSequentially(),
-        ),
-      ],
-      child: const AppView(),
-    );
+    return const AppView();
   }
 }
 
@@ -48,8 +33,6 @@ class AppView extends StatelessWidget {
         ),
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
       home: GameWidget(game: FietrisGame()),
     );
   }
