e53e56308bd23168e1e93fb1bdf704c3e693c6d7
mehmet fiskindal <developersailor@gmail.com>
2025-04-18 17:43:17 +0300

    init

---
+++ android/app/src/main/kotlin/com/fiskindal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fiskindal.fietris
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:fietris/app/app.dart';
+import 'package:fietris/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
